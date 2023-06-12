d6e432403af3406ead4d9c4e5a815543533e9abe
Ravindra Pawar <ravindra.pawar99@abellgroup.com>
2023-06-12 21:15:19 +0530

    Simple Splash functions

--- app/src/main/java/com/mobileappguru/androidtraining/SplashScreenActivity.kt
+++ app/src/main/java/com/mobileappguru/androidtraining/SplashScreenActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.Window
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,0)
         setContentView(R.layout.activity_splash_screen)
+
 
         Log.i(localClassName,"On Splash Screen")
         Handler().postDelayed({
